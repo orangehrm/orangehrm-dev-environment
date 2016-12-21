db8c673b86344ba03b9bb9c8a53e629c19ff0c1c
ChanakaR <bmcrathnayaka@gmail.com>
2016-12-21 13:00:13 +0530

    added .gitattributes and mount volumes for logs

--- tests/acceptance/UbuntuContainerCest.php
+++ tests/acceptance/UbuntuContainerCest.php
@@ -11,15 +11,32 @@ class UbuntuContainerCest
     }
 
     // tests
-    public function ContainerTest(AcceptanceTester $I){
-        $I->wantTo("verify ubuntu container up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} dev_web");
+
+
+    public function mySqlContainerTest(AcceptanceTester $I){
+        $I->wantTo("verify mysql container is up and running");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} dev_mysql");
         $I->seeInShellOutput("true");
     }
 
-    public function mySqlContainerTest(AcceptanceTester $I){
-        $I->wantTo("verify mysql container up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} dev_mysql");
+    public function phpmyadminContainerTest(AcceptanceTester $I){
+        $I->wantTo("verify phpmyadmin container is up and running");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} dev_phpmyadmin");
+        $I->seeInShellOutput("true");
+    }
+
+//    public function phpmyadminLinkTest(AcceptanceTester $I){
+//        $I->wantTo("verify that phpmyadmin is linked with mysql container properly");
+//        $I->amOnPage("/");
+//        $I->fillField('username','root');
+//        $I->fillField('password','1234');
+//        $I->click('Go');
+//        $I->seeInCurrentUrl('/index.php?token');
+//    }
+
+    public function ContainerTest(AcceptanceTester $I){
+        $I->wantTo("verify ubuntu container up and running");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} dev_web");
         $I->seeInShellOutput("true");
     }
 
@@ -37,7 +54,7 @@ class UbuntuContainerCest
 
     public function mysqlServerTest(AcceptanceTester $I){
         $I->wantTo("verify mysql server container is linked properly");
-            $I->runShellCommand("docker exec dev_web ping db -c 2");
+        $I->runShellCommand("docker exec dev_web ping db -c 2");
         $I->seeInShellOutput('2 packets transmitted, 2 received');
     }
 }
