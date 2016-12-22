e9e175bf01593e4ecdd098f8c7e8a5f8e5e2bb59
ChanakaR <bmcrathnayaka@gmail.com>
2016-12-22 17:10:08 +0530

    added test cases for testing dependency libraries

--- tests/acceptance/UbuntuContainerCest.php
+++ tests/acceptance/UbuntuContainerCest.php
@@ -23,6 +23,86 @@ class UbuntuContainerCest
         $I->seeInShellOutput('2 packets transmitted, 2 received');
     }
 
+    public function apacheInstalledTest(AcceptanceTester $I){
+        $I->wantTo("verify apache is installed in the container");
+        $I->runShellCommand("docker exec dev_web apache2 -v");
+        $I->seeInShellOutput('Server version: Apache/2.4.7');
+    }
+
+    public function apacheRunningTest(AcceptanceTester $I){
+        $I->wantTo("verify apache is up and running in the container");
+        $I->runShellCommand("docker exec dev_web service apache2 status");
+        $I->seeInShellOutput('apache2 is running');
+    }
+
+    public function cronTest(AcceptanceTester $I){
+        $I->wantTo("verify cron is installed in the container");
+        $I->runShellCommand("docker exec dev_web apt list --installed | grep cron");
+        $I->seeInShellOutput('cron/now 3.0');
+    }
+
+    public function mysqlClientTest(AcceptanceTester $I){
+        $I->wantTo("verify mysql-client is installed in the container");
+        $I->runShellCommand("docker exec dev_web apt list --installed | grep mysql-client");
+        $I->seeInShellOutput('mysql-client/now 5.5');
+    }
+
+    public function libreOfficeTest(AcceptanceTester $I){
+        $I->wantTo("verify LibreOffice is installed in the container");
+        $I->runShellCommand("docker exec dev_web libreoffice --version");
+        $I->seeInShellOutput('LibreOffice 4.2');
+    }
+
+    public function libpng12Test(AcceptanceTester $I){
+        $I->wantTo("verify libpng12-dev is installed in the container");
+        $I->runShellCommand("docker exec dev_web apt list --installed");
+        $I->seeInShellOutput('libpng12-dev');
+    }
+
+    public function propplerUtilTest(AcceptanceTester $I){
+        $I->wantTo("verify poppler-util is installed in the container");
+        $I->runShellCommand("docker exec dev_web apt list --installed | grep poppler-util");
+        $I->seeInShellOutput('poppler-util');
+    }
+
+    public function zipTest(AcceptanceTester $I){
+        $I->wantTo("verify zip library is installed in the container");
+        $I->runShellCommand("docker exec dev_web zip -v");
+        $I->seeInShellOutput('Zip 3');
+    }
+
+    public function unzipTest(AcceptanceTester $I){
+        $I->wantTo("verify UnZip library is installed in the container");
+        $I->runShellCommand("docker exec dev_web unzip -v");
+        $I->seeInShellOutput('UnZip 6');
+    }
+
+    public function phpunitTest(AcceptanceTester $I){
+        $I->wantTo("verify phpunit library is installed in the container");
+        $I->runShellCommand("docker exec dev_web phpunit --version");
+        $I->seeInShellOutput('PHPUnit 3');
+    }
+
+    public function gitTest(AcceptanceTester $I){
+        $I->wantTo("verify git is installed in the container");
+        $I->runShellCommand("docker exec dev_web git --version");
+        $I->seeInShellOutput('git version 1');
+    }
+
+
+    public function curlTest(AcceptanceTester $I){
+        $I->wantTo("verify curl is installed in the container");
+        $I->runShellCommand("docker exec dev_web curl --version");
+        $I->seeInShellOutput('curl 7.35');
+    }
+
+    public function phpTest(AcceptanceTester $I){
+        $I->wantTo("verify php 5.5 is installed in the container");
+        $I->runShellCommand("docker exec dev_web php --version");
+        $I->seeInShellOutput('PHP 5.5.9');
+    }
+
+
     public function nodeTest(AcceptanceTester $I){
         $I->wantTo("verify node v4 is installed in the container");
         $I->runShellCommand("docker exec dev_web node -v");
@@ -46,14 +126,6 @@ class UbuntuContainerCest
         $I->runShellCommand("docker exec dev_web bower --version");
         $I->seeInShellOutput('1');
     }
-
-    public function phpTest(AcceptanceTester $I){
-        $I->wantTo("verify php 5.5 is installed in the container");
-        $I->runShellCommand("docker exec dev_web php --version");
-        $I->seeInShellOutput('PHP 5.5.9');
-    }
-
-
 
 
 }
