34c209ef48a367e9524340dabd52a3e4f0af09e7
ChanakaR <bmcrathnayaka@gmail.com>
2016-12-27 12:17:56 +0530

    added acceptance test

---
+++ tests/acceptance/DevEnvironmentCest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class DevEnvironmentCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->comment("Cloning project into /var/www/html");
+        $I->runShellCommand("docker exec dev_web git clone https://github.com/ChanakaR/php-simple.git /var/www/html/php-simple");
+        $I->runShellCommand('docker exec dev_web chmod 777 -R /var/www/html');
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+        $I->comment("remove the project directory from /var/www/html");
+        $I->runShellCommand('docker exec dev_web rm -rf /var/www/html/php-simple');
+    }
+
+    public function devWebTest(AcceptanceTester $I){
+        $I->wantTo("verify dev environment is working properly with a php application");
+        $I->runShellCommand("docker exec dev_web php /var/www/html/php-simple/app.php");
+        $I->cantSeeInShellOutput("false");
+    }
+}
