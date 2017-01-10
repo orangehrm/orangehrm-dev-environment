61ce4099acdd220030158d67abefa68b5d30eef4
rshariffdeen <rshariffdeen@gmail.com>
2017-01-10 08:12:21 +0530

    correcting container name in test cases

--- tests/functional/UbuntuContainerCest.php
+++ tests/functional/UbuntuContainerCest.php
@@ -14,109 +14,109 @@ class UbuntuContainerCest
 
   public function ContainerTest(AcceptanceTester $I){
         $I->wantTo("verify ubuntu container up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} qa_web_55");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} dev_web");
         $I->seeInShellOutput("true");
     }
 
     public function apacheInstalledTest(AcceptanceTester $I){
         $I->wantTo("verify apache is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 apache2 -v");
+        $I->runShellCommand("docker exec dev_web apache2 -v");
         $I->seeInShellOutput('Server version: Apache/2.4.10');
     }
 
     public function apacheRunningTest(AcceptanceTester $I){
         $I->wantTo("verify apache is up and running in the container");
-        $I->runShellCommand("docker exec qa_web_55 service apache2 status");
+        $I->runShellCommand("docker exec dev_web service apache2 status");
         $I->seeInShellOutput('apache2 is running');
     }
 
     public function cronTest(AcceptanceTester $I){
         $I->wantTo("verify cron is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 apt list --installed | grep cron");
+        $I->runShellCommand("docker exec dev_web apt list --installed | grep cron");
         $I->seeInShellOutput('cron/now 3.0');
     }
 
     public function mysqlClientTest(AcceptanceTester $I){
         $I->wantTo("verify mysql-client is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 apt list --installed | grep mysql-client");
+        $I->runShellCommand("docker exec dev_web apt list --installed | grep mysql-client");
         $I->seeInShellOutput('mysql-client/now 5.5');
     }
 
     public function libreOfficeTest(AcceptanceTester $I){
         $I->wantTo("verify LibreOffice is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 libreoffice --version");
+        $I->runShellCommand("docker exec dev_web libreoffice --version");
         $I->seeInShellOutput('LibreOffice 4.3.3.2');
     }
 
     public function propplerUtilTest(AcceptanceTester $I){
         $I->wantTo("verify poppler-util is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 apt list --installed | grep poppler-util");
+        $I->runShellCommand("docker exec dev_web apt list --installed | grep poppler-util");
         $I->seeInShellOutput('poppler-util');
     }
 
     public function zipTest(AcceptanceTester $I){
         $I->wantTo("verify zip library is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 zip -v");
+        $I->runShellCommand("docker exec dev_web zip -v");
         $I->seeInShellOutput('Zip 3');
     }
 
     public function unzipTest(AcceptanceTester $I){
         $I->wantTo("verify UnZip library is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 unzip -v");
+        $I->runShellCommand("docker exec dev_web unzip -v");
         $I->seeInShellOutput('UnZip 6');
     }
 
     public function phpunitTest(AcceptanceTester $I){
         $I->wantTo("verify phpunit library is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 phpunit --version");
+        $I->runShellCommand("docker exec dev_web phpunit --version");
         $I->seeInShellOutput('PHPUnit 4.2.6');
     }
 
     public function gitTest(AcceptanceTester $I){
         $I->wantTo("verify git is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 git --version");
+        $I->runShellCommand("docker exec dev_web git --version");
         $I->seeInShellOutput('git version 2.1.4');
     }
 
     public function curlTest(AcceptanceTester $I){
         $I->wantTo("verify curl is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 curl --version");
+        $I->runShellCommand("docker exec dev_web curl --version");
         $I->seeInShellOutput('curl 7.38');
     }
 
     public function phpTest(AcceptanceTester $I){
         $I->wantTo("verify php 5.5 is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 php --version");
+        $I->runShellCommand("docker exec dev_web php --version");
         $I->seeInShellOutput('PHP 5.5');
     }
 
     public function nodeTest(AcceptanceTester $I){
         $I->wantTo("verify node v4 is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 node -v");
+        $I->runShellCommand("docker exec dev_web node -v");
         $I->seeInShellOutput('v4');
     }
 
     public function npmTest(AcceptanceTester $I){
         $I->wantTo("verify npm is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 npm --version");
+        $I->runShellCommand("docker exec dev_web npm --version");
         $I->seeInShellOutput("2");
     }
 
     public function nodemonTest(AcceptanceTester $I){
         $I->wantTo("verify nodemon is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 nodemon");
+        $I->runShellCommand("docker exec dev_web nodemon");
         $I->seeInShellOutput('Usage: nodemon');
     }
 
     public function bowerTest(AcceptanceTester $I){
         $I->wantTo("verify bower is installed in the container");
-        $I->runShellCommand("docker exec qa_web_55 bower --version");
+        $I->runShellCommand("docker exec dev_web bower --version");
         $I->seeInShellOutput('1');
     }
 
     public function phpModuleTest(AcceptanceTester $I){
             $I->wantTo("verify required php modules are available");
-            $I->runShellCommand("docker exec qa_web_55 php -m");
+            $I->runShellCommand("docker exec dev_web php -m");
             $I->seeInShellOutput('bcmath');
             $I->seeInShellOutput('calendar');
             $I->seeInShellOutput('Core');
