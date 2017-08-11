c36ba4fbcdfdf42dd276d28f31045089c4a9b2d1
thulana <thulanakannangara@gmail.com>
2017-08-11 14:31:39 +0530

    configure test cases

--- tests/functional/WebContainerCest.php
+++ tests/functional/WebContainerCest.php
@@ -15,7 +15,7 @@ class WebContainerCest
     public function checkConnectionWithDB(FunctionalTester $I){
         $I->wantTo("verify mysql 5.5 container is linked with php 5.6 container properly");
         $I->runShellCommand("docker exec dev_web ping db -c 2");
-        $I->seeInShellOutput('2 packets transmitted, 2 packets received');
+        $I->seeInShellOutput('2 packets transmitted, 2 received');
     }
 
 }
