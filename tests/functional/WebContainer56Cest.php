39329a5a3d746a4790aaafde3651d8dc583755ac
thulana <thulanakannangara@gmail.com>
2017-09-27 13:41:26 +0530

    change test cases

--- tests/functional/WebContainer56Cest.php
+++ tests/functional/WebContainer56Cest.php
@@ -25,7 +25,7 @@ class WebContainer56Cest
     public function checkConnectionWithDB57(FunctionalTester $I){
         $I->wantTo("verify mysql 5.7 container is linked with php 5.6 container properly");
         $I->runShellCommand("docker exec dev_web_56 ping db57 -c 2");
-        $I->seeInShellOutput('2 packets transmitted, 2 packets received');
+        $I->seeInShellOutput('2 packets transmitted, 2 received');
     }
 
     public function checkConnectionWithDB101(FunctionalTester $I){
