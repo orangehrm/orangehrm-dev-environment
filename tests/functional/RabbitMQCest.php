b2302340c619615d72fa4c58233674d742ecbfc4
thulana <thulanakannangara@gmail.com>
2017-10-24 09:59:56 +0530

    fix issue in test

--- tests/functional/RabbitMQCest.php
+++ tests/functional/RabbitMQCest.php
@@ -16,7 +16,7 @@ class RabbitMQCest
         $I->comment("test rabbit mq server");
         $I->runShellCommand('docker exec dev_web_56 bash -c " composer install"');
         $I->runShellCommand('docker exec dev_web_56 bash -c "php send.php"');
-        $I->runShellCommand('docker exec phantom_web bash -c "php recieve.php"');
+        $I->runShellCommand('docker exec dev_web_56 bash -c "php recieve.php"');
         $I->seeInShellOutput('Hello World!');
     }
 
