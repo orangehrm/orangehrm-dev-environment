5ea4826b80ff33cea616c438e733411eb89c6c05
thulana <thulanakannangara@gmail.com>
2017-11-22 14:55:08 +0530

    add oci8 php module

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -137,4 +137,9 @@ class WebContainerCest
         $I->seeInShellOutput('1');
     }
 
+    public function checkOci8PHPmodule(UnitTester $I){
+        $I->wantTo("verify php module oci8 is installed in the container");
+        $I->runShellCommand("docker exec php -m");
+        $I->seeInShellOutput('oci8');
+    }
 }
