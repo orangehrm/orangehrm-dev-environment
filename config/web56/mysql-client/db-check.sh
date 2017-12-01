a010536745c15fbf2635d5e36173ab938fd1064f
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2017-12-01 13:18:08 +0530

    db check script added

---
+++ config/web56/mysql-client/db-check.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+while true; do
+ NMAP=$(nmap -P0 -sT -p3306 db55)
+ if [[ $NMAP == *"Host is up"* && $NMAP == *"open"* ]]; then
+    echo "connection!!!"
+    break
+ fi
+
+ sleep 5
+ echo "waiting for connection"
+done
