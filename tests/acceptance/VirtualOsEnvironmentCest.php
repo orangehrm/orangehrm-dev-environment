dcea3fa60f02310ed8cdf094b41d7dc4a451c5ca
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2017-12-01 10:54:52 +0530

    test cases updated

--- tests/acceptance/VirtualOsEnvironmentCest.php
+++ tests/acceptance/VirtualOsEnvironmentCest.php
@@ -34,7 +34,6 @@ class VirtualOsEnvironmentCest
         $I->runShellCommand('docker exec dev_web_56 bash -c "service httpd restart"');
         $I->runShellCommand('docker exec dev_web_56 bash -c "service httpd status -l"');
         $I->runShellCommand('docker exec dev_web_56 bash -c "ls /var/log/httpd/OHRMStandalone"');
-        $I->runShellCommand('docker exec dev_web_56 bash -c "cat /var/log/httpd/OHRMStandalone"');
         $I->runShellCommand('docker exec dev_web_56 bash -c "cd /var/log/httpd/OHRMStandalone/opensource/ && cat %0-error.log && cat %0-access.log"');
     }
 
