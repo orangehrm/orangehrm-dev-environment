7cdb69f38580cbbc112e31a358b02484eabab03d
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2017-12-01 11:06:32 +0530

    test cases updated

--- tests/acceptance/VirtualOsEnvironmentCest.php
+++ tests/acceptance/VirtualOsEnvironmentCest.php
@@ -19,7 +19,7 @@ class VirtualOsEnvironmentCest
     {
         $I->comment("Verify Header values");
 
-//        $I->runShellCommand('docker exec dev_web_56 bash -c "curl -k -i https://orangehrm-os.orangehrm.com"');
+        $I->runShellCommand('docker exec dev_web_56 bash -c "curl -v 127.0.0.1"');
 //        $I->seeInShellOutput("Content-Security-Policy: default-src 'self' *.projects-abroad.net native.testing.equest.com www.youtube.com sandbox.e-signlive.com player.vimeo.com fonts.googleapis.com fonts.gstatic.com 'unsafe-inline' 'unsafe-eval';img-src * 'self' data: blob: ;font-src 'self' fonts.gstatic.com sandbox.e-signlive.com data:");
 //        $I->seeInShellOutput("X-XSS-Protection: 1; mode=block");
 //        $I->seeInShellOutput("X-Content-Type-Options: nosniff");
