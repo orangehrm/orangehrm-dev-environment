7ab0896766f2c8c9d498bb83780c39b98171fd01
charithra7 <cklzem7@gmail.com>
2018-02-27 18:48:20 +0530

    new_extentions

--- tests/functional/PhpmyadminContainerCest.php
+++ tests/functional/PhpmyadminContainerCest.php
@@ -18,5 +18,19 @@ class PhpmyadminContainerCest
         $I->seeInShellOutput('1 packets transmitted, 1 packets received');
     }
 
+    public function checkAstExtention(FunctionalTester $I){
+        $I->wantTo("verify ast extention");
+        $I->runShellCommand("docker exec dev_web bash -c 'php -m | grep ast'");
+      //  $I->runShellCommand('docker exec dev_web bash -c "mkdir -p orangehrm/symfony/web"');
+
+        $I->seeInShellOutput('ast');
+    }
+
+    public function checkPhanExtention(FunctionalTester $I){
+        $I->wantTo('Verify the phan');
+        $I->runShellCommand("docker exec dev_web bash -c 'phan -v | grep Phan'");   
+        $I->seeInShellOutput('Phan');
+    }
+
 
 }
