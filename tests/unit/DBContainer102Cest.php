94f466d94e99bda3791179e51eef9f2875015c17
charithra7 <cklzem7@gmail.com>
2018-03-02 16:20:51 +0530

    tests were updated

--- tests/unit/DBContainer102Cest.php
+++ tests/unit/DBContainer102Cest.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class DBContainer101Cest
+class DBContainer102Cest
 {
     public function _before(UnitTester $I)
     {
@@ -12,21 +12,21 @@ class DBContainer101Cest
     }
 
     public function checkContainerRunning(UnitTester $I){
-        $I->wantTo("verify mariadb 10.1 container is up and running");
-        $I->runShellCommand("docker inspect -f {{.State.Running}} dev_mariadb_101");
+        $I->wantTo("verify mariadb 10.2 container is up and running");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} dev_mariadb_102");
         $I->seeInShellOutput("true");
     }
 
     public function checkMySQLServiceIsRunning(UnitTester $I){
-        $I->wantTo("verify mariadb 10.1 service is up and running");
+        $I->wantTo("verify mariadb 10.2 service is up and running");
         $I->runShellCommand("ping -c 30 localhost");
-        $I->runShellCommand("docker exec dev_mariadb_101 mysqladmin -uroot -p1234 status");
+        $I->runShellCommand("docker exec dev_mariadb_102 mysqladmin -uroot -p1234 status");
         $I->seeInShellOutput("Uptime");
     }
 
     public function checkMySQLConfigurations(UnitTester $I){
         $I->wantTo("verify my.cnf configuration is loaded");
-        $I->runShellCommand("docker exec dev_mariadb_101 mysql -uroot -p1234 -se 'show variables'");
+        $I->runShellCommand("docker exec dev_mariadb_102 mysql -uroot -p1234 -se 'show variables'");
         $I->seeInShellOutput("event_scheduler	ON");
         $I->seeInShellOutput("innodb_log_buffer_size	8388608");
         $I->seeInShellOutput("innodb_buffer_pool_size	2147483648");
