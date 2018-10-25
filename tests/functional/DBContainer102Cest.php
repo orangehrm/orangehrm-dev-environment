c9c3a32f7b2bc8ffd249b840935334459f31b81b
charithra7 <cklzem7@gmail.com>
2018-10-25 11:29:08 +0530

    functional test update

--- tests/functional/DBContainer102Cest.php
+++ tests/functional/DBContainer102Cest.php
@@ -13,8 +13,8 @@ class DBContainer102Cest
 
     public function MariaDBTest(FunctionalTester $I){
         $I->wantTo("verify mariadb 10.2  container is working properly");
-        $I->runShellCommand("docker exec dev_mariadb_102 ping db102 -c 1");
-        $I->seeInShellOutput('64 bytes from 10.5.0.7: icmp_seq=0 ttl=64');
+        $I->runShellCommand("docker exec dev_web_71 ping db102 -c 1");
+        $I->seeInShellOutput('64 bytes from dev_mariadb_102.orangehrm-dev-environment_ohrmdevnet (10.5.0.7): icmp_seq=1 ttl=64');
     }
 
 }
